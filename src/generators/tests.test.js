11b7755f976347687d2e670307b6e75e75772f52
Isac Petinate <idpetinate@gmail.com>
2024-04-11 17:02:27 -0300

    fix: remove unnecessary import

--- src/generators/tests.test.js
+++ src/generators/tests.test.js
@@ -1,8 +1,6 @@
 import { describe, it } from "node:test";
 import assert from "node:assert/strict";
 
-import { FrameworkEnum } from "../enums/frameworks.js";
-
 /**
  * @type {import("./components.js").Answers}
  */
