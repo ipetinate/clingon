af925f8499dfc73bfeca8e2e248abdc795ba4561
Isac Petinate <idpetinate@gmail.com>
2024-04-17 06:11:53 -0300

    refactor: move code inside utils to self generators

--- src/generators/tests.js
+++ src/generators/tests.js
@@ -1,11 +1,14 @@
+import { FrameworkEnum } from "../enums/frameworks.js";
+
+import { unitTestTemplates } from "../constants/templates.js";
+
 import { compose } from "../utils/compose.js";
 import { capitalizeLetter } from "../utils/string.js";
 import {
-  defineComponentTemplate,
-  generateComponentFile,
-  getTemplateContent,
-  replaceAllComponentTextOccurrences,
-} from "../utils/tests.js";
+  makeFileExtension,
+  createFileWithContent,
+  readFileContent,
+} from "../utils/file.js";
 
 /**
  * Component generator
@@ -28,4 +31,84 @@ export function generateTests(answers) {
   if (error) {
     console.info(`Error on create ${answers.testPostfix}, try again`);
   }
+}
+
+/**
+ * Get component template details
+ *
+ * @param {Answers & { path: string }} data - Data to compose component
+ * @returns {() => Answers & { templatePath: string }}
+ */
+export function defineComponentTemplate(data) {
+  return () => {
+    /**
+     * @type {"js" | "ts"}
+     */
+    const variant = data.typescript ? "ts" : "js";
+
+    /**
+     * Template path from path's dictionary
+     */
+    const templatePath = unitTestTemplates.generic[variant].unit;
+
+    return { ...data, templatePath };
+  };
+}
+
+/**
+ * Get template content from file
+ *
+ * @param {Answers & { templatePath: string }} data - Data to compose component
+ * @returns {() => Answers & {
+ *    templatePath: string,
+ *    fileContent: string
+ *  }}
+ */
+export function getTemplateContent(data) {
+  const fileContent = readFileContent(data.templatePath);
+
+  return { ...data, fileContent };
+}
+
+/**
+ * Get template content from file
+ *
+ * @param {Answers & {
+ *    templatePath: string,
+ *    fileContent: string
+ *  }} data - Data to compose component
+ * @returns {() => data}
+ */
+export function replaceAllComponentTextOccurrences(data) {
+  data.fileContent = data.fileContent.replace("ResourceName", data.name);
+
+  return data;
+}
+
+/**
+ * Get template content from file
+ *
+ * @param {Answers & {
+ *    templatePath: string,
+ *    fileContent: string
+ *  }} data - Data to compose component
+ * @returns {() => string}
+ */
+export function generateComponentFile(data) {
+  const extension = makeFileExtension({
+    typescript: data.typescript,
+    postfix: data.testPostfix,
+    withJsx: data.framework === FrameworkEnum.react,
+  });
+
+  const fileName = `${data.name}.${extension}`;
+  const pathWithFileName = `${data.path}/${fileName}`;
+
+  const success = createFileWithContent(pathWithFileName, data.fileContent);
+
+  return {
+    success,
+    error: !success,
+    path: pathWithFileName,
+  };
 }
--- src/utils/component.test.js
+++
@@ -1,6 +0,0 @@
-import { describe, it, todo } from "node:test";
-import assert from "node:assert/strict";
-
-describe.todo("Component Generator Utils", () => {
-  describe.todo("createComponent util", () => {});
-});
