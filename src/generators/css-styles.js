29dbd3a4e416acd3d5c7e272e2b09c1a61bacd3c
Isac Petinate <idpetinate@gmail.com>
2024-05-01 09:11:01 -0300

    fix: bypass style generator when is tailwind inline

--- src/generators/css-styles.js
+++ src/generators/css-styles.js
@@ -19,7 +19,9 @@ import { StylePostfixEnum } from '../enums/postfixes.js'
  * @param {Answers & { path: string }} answers Answers prompted to the user
  */
 export function generateStyle(answers) {
-  const { success, error, path } = compose(
+  if (answers.cssFramework === 'tailwind_inline') return
+
+  const { success, path } = compose(
     defineStyleTemplate(answers),
     getTemplateContent,
     replaceAllFunctionTextOccurrences,
