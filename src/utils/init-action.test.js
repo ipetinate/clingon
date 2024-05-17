b4d4a69adf54c1f7e1b17e33a4a4dda6d66ab3d2
Isac Petinate <idpetinate@gmail.com>
2024-05-17 16:41:47 -0300

    fix: imports and workflow

--- src/utils/init-action.test.js
+++ src/utils/init-action.test.js
@@ -1,9 +1,8 @@
 import fs from 'node:fs'
-import assert from 'node:assert'
+import assert from 'node:assert/strict'
 
 import { describe, it, mock } from 'node:test'
 
-import { initAction } from '../actions/init.js'
 import {
   checkIfPresetFolderAlreadyExists,
   createFileIfNotExists,
